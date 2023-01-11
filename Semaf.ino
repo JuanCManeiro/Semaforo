23a4996b739a8e59ea7278bfa93e21b8292c137f
JuanCManeiro <118885303+JuanCManeiro@users.noreply.github.com>
2023-01-11 20:42:21 +0100

    cargado texto con pulsador para peatons
    moito ollo cos espacios usara barra ou punto ou todo xunto x ex.

--- Semaf.ino
+++ Semaf.ino
@@ -62,3 +62,53 @@ void loop()
   digitalWrite(10, HIGH);
   delay(2000); // Wait for 2000 millisecond(s)
 }//actualizado 
+/***
+***/
+// C++ code
+//
+int peaton = 0;
+int pulsadorpeaton = 0;/**moito ollo aqui, puxen pulsador peaton con espacio e mais
+adiante no o recoñecia, no momento que o modifiquei e o puxen todo xunto anulou o fallo en voop**/
+void setup()
+{
+  pinMode(9, INPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(10, OUTPUT);
+
+  Serial.println("hello world");
+}
+
+void loop()
+{
+  pulsadorpeaton = digitalRead(9);
+  if (pulsadorpeaton == HIGH) {
+    digitalWrite(11, HIGH);
+    delay(2000); // Wait for 2000 millisecond(s)
+    digitalWrite(12, HIGH);
+    delay(6000); // Wait for 6000 millisecond(s)
+  } else {
+    Serial.println("reinicio");
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(11, HIGH);
+    digitalWrite(10, HIGH);
+    delay(4000); // Wait for 4000 millisecond(s)
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(11, LOW);
+    delay(1000); // Wait for 1000 millisecond(s)
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(11, HIGH);
+    digitalWrite(10, LOW);
+    delay(3000); // Wait for 3000 millisecond(s)
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(12, HIGH);
+    digitalWrite(11, HIGH);
+    digitalWrite(10, HIGH);
+    delay(2000); // Wait for 2000 millisecond(s)
+  }
+}
